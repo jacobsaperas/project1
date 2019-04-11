ed35de4e9981ee360336c4fa18d026d889449709
Jacob Saperas <jacobsaperas@outlook.com>
2019-04-11 07:01:29 +0000

    updated

--- main.c
+++ main.c
@@ -10,9 +10,9 @@ int main()
 {
 	/* to input characters a string needs to be used.
 	When using characters the data type "char" must be used.*/
-	char text[100] = "XYZ";  // declares a string of length 100.
+	char text[100];  // declares a string of length 100.
 	int index;  // "int index" will represent the index (ASCII placement) of a character.
-	int n = 1; // variable "n" will be used to represent the letter's rotation.
+	int n; // variable "n" will be used to represent the letter's rotation.
 	
 	printf("Message being encrytped: "); // prints text between " " to screen.
 	gets(text);                         // the "gets" function basically works as a "scanf" but includes spaces.
@@ -22,23 +22,41 @@ int main()
 	scanf("%d", &n);  // The "scanf" function holds an integer of size "n" and stores it in the memory adress.
 	//.... The number inputed represents the letter's rotation. 	
 	
-	/* The code below will encrypt text by shifting letters "n" times to the right.*/
+	/* The function below will encrypt text by shifting letters "n" times to the right.*/
 	
 	for (index = 0; text[index] != '\0'; ++index) { // This will run every character...
 	// .... from the start of the string to the end.
 		
 		if (text[index] >= 'A' && text[index] <= 'Z') { // if a character of the input was an...
 		//... upper case letter it will go through this if statement.
-			text[index] = text[index] + n;  // shifts letter from "text[index]", "n" places.
-		
-            if (text[index] > 'Z') { // If the letter's ASCII position exceeds 'Z'...
+			text[index] = text[index] + n;  // shifts letter from "text[index]" plus "n" places.
+		}
+			if (text[index] > 'Z') { // If the letter's ASCII position exceeds 'Z'...
 			//... i.e Z with rotation of 2, then the character will go through this if statement.
 				text[index] = text[index] - 26; // since there are 26 letters, -26 will take the ...
-				// letter back 26 places in it's ASCII position. i.e Z with rotation 2 would become B.
+				// letter backward 26 places in it's ASCII position. i.e 'Z' with forward 2 rotations ...
+				// would become whatever the second previous character was on the ASCII table. ....
+				// Then 26 is subtracted to it's ASCII position and it moves backwards to 'B'.
 			}
-		}
-
+			
 	}
+    /* The function below will decrypt text by shifting letters "n" times to the left */
+	
+	for (index = 0; text[index] != '\0'; ++index) { 
+		
+		if (text[index] >= 'A' && text[index] <= 'Z') { 
+			text[index] = text[index] - n;  // shifts letter from "text[index]" minus "n" places.
+		}
+			if (text[index] < 'A') { 
+				text[index] = text[index] + 26; // since there are 26 letters, +26 will take the ...
+				// letter forward 26 places in it's ASCII position. i.e 'A' with 1 backward rotation ...
+				// would become whatever the previous character is on the ASCII table. ....
+				// Then 26 is added to it's ASCII position and it moves forward to 'Z'.
+			}
+
+			
+	}
+
 	
 	printf("Encrypted message: %s", text);
 	
