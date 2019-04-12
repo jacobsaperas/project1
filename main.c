bb62d60b16e41a6f6ba0b83b1506755a9496ec01
Jacob Saperas <jacobsaperas@outlook.com>
2019-04-12 01:42:39 +0000

    Yew i did it

--- main.c
+++ main.c
@@ -6,59 +6,61 @@ i.e. 5: 56 will be printed as 5:56
 The inputed letters will then be shifted right "n" spaces if it's an encryption,
 or left "n" spaces if it's a decryption.
 The value of "n" can be inputed depending on the rotation of letters you want*/
+
+void encryption(char text[]); // this function will be called when encrypting
+void decryption(char text[]); // this function will be called when decrypting
+
+/* this main function lets you enter your message that you want to encrypt or decrypt.
+ You then choose to encrypt or decrypt.*/
 int main()
 {
-	/* to input characters a string needs to be used.
-	When using characters the data type "char" must be used.*/
-	char text[100];  // declares a string of length 100.
-	int index;  // "int index" will represent the index (ASCII placement) of a character.
-	int n; // variable "n" will be used to represent the letter's rotation.
+	int flag;
+	char text[100];
+	printf("enter message: ");
+	gets(text);
+	printf("Press 0 to enrypt this message or press 2 to decrypt: ");
+	scanf("%d", &flag);
 	
-	printf("Message being encrytped: "); // prints text between " " to screen.
-	gets(text);                         // the "gets" function basically works as a "scanf" but includes spaces.
-	//... This is useful since when inputing text it is seen as one string of characters.
-	//.... The "gets" function will hold the text inputed onto the screen.
-	printf("Enter rotation: "); // acts as a prompt to enter rotation to the screen
-	scanf("%d", &n);  // The "scanf" function holds an integer of size "n" and stores it in the memory adress.
-	//.... The number inputed represents the letter's rotation. 	
-	
-	/* The function below will encrypt text by shifting letters "n" times to the right.*/
-	
-	for (index = 0; text[index] != '\0'; ++index) { // This will run every character...
-	// .... from the start of the string to the end.
-		
-		if (text[index] >= 'A' && text[index] <= 'Z') { // if a character of the input was an...
-		//... upper case letter it will go through this if statement.
-			text[index] = text[index] + n;  // shifts letter from "text[index]" plus "n" places.
-		}
-			if (text[index] > 'Z') { // If the letter's ASCII position exceeds 'Z'...
-			//... i.e Z with rotation of 2, then the character will go through this if statement.
-				text[index] = text[index] - 26; // since there are 26 letters, -26 will take the ...
-				// letter backward 26 places in it's ASCII position. i.e 'Z' with forward 2 rotations ...
-				// would become whatever the second previous character was on the ASCII table. ....
-				// Then 26 is subtracted to it's ASCII position and it moves backwards to 'B'.
-			}
-			
+	if (flag < 1) {
+	    encryption(text);
 	}
-    /* The function below will decrypt text by shifting letters "n" times to the left */
-	
-	for (index = 0; text[index] != '\0'; ++index) { 
-		
-		if (text[index] >= 'A' && text[index] <= 'Z') { 
-			text[index] = text[index] - n;  // shifts letter from "text[index]" minus "n" places.
-		}
-			if (text[index] < 'A') { 
-				text[index] = text[index] + 26; // since there are 26 letters, +26 will take the ...
-				// letter forward 26 places in it's ASCII position. i.e 'A' with 1 backward rotation ...
-				// would become whatever the previous character is on the ASCII table. ....
-				// Then 26 is added to it's ASCII position and it moves forward to 'Z'.
-			}
+	else if (flag > 1) {
+	    decryption(text);
+    }
 
-			
-	}
-
-	
-	printf("Encrypted message: %s", text);
-	
 	return 0;
 }
+	
+	void encryption(char text[100]) {
+	    int index, n;
+	    printf("\nEnter rotation: ");
+	    scanf("%d", &n);  
+	    for (index = 0; text[index] != '\0'; ++index) { 
+		  
+	    	if (text[index] >= 'A' && text[index] <= 'Z') { 
+			text[index] = text[index] + n;
+		}
+			if (text[index] > 'Z') { 
+				text[index] = text[index] - 26; 
+			}
+	    }
+	    printf("encrypted text: ");
+    	puts(text);
+	}
+	
+    void decryption(char text[100]) {
+        int index, n;
+        printf("\nEnter rotation: "); 
+	    scanf("%d", &n);  
+	    for (index = 0; text[index] != '\0'; ++index) { 
+		
+	    	if (text[index] >= 'A' && text[index] <= 'Z') { 
+			text[index] = text[index] - n; 
+		}
+			if (text[index] < 'A') { 
+				text[index] = text[index] + 26; 
+			}
+    	}
+    	printf("decrypted text: ");
+    	puts(text);
+    }
